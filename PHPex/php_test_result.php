78be7a3230480ee4b939cb239c5707da29a9e59c
gold-succ-requiem <56791062+gold-succ-requiem@users.noreply.github.com>
2019-11-04 19:26:10 +1100

    Add files via upload

---
+++ PHPex/php_test_result.php
@@ -0,0 +1,44 @@
+
+<!DOCTYPE HTML>  
+<html>
+<head>
+<style>
+.error {color: #FF0000;}
+</style>
+</head>
+<body>  
+
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $website;
+echo "<br>";
+echo $comment;
+echo "<br>";
+echo $gender;
+?>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
